d95b3e501702ccfef0baf02b1dd6522b063143c9
kaolss <karin@kobotolo.se>
2014-09-12 14:03:30 +0200

    use of settings

--- wp-content/themes/bootstrap/index.php
+++ wp-content/themes/bootstrap/index.php
@@ -7,30 +7,27 @@
  */
 get_header(); 
 echo 'index.php';?>
-<div class="container-fluid">
+<div class="site-inner">
     <div class="container">
-    <div class="row site-inner">
-  <div class="col-md-8">
-      
-	<?php if ( have_posts() ) : 
-            do_action( 'loop_header' ); 
-            while ( have_posts() ) : the_post(); ?>
+	<?php do_action('sidebar_left'); ?>  	
+	<div class="col-md-8 main-content">
+	    <?php if ( have_posts() ) : 
+		do_action( 'loop_header' ); 
+		 while ( have_posts() ) : the_post(); ?>
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
                     <?php do_action( 'entry_data' ); ?>
                     <?php do_action( 'entry_thumbnail' ); ?>
                     <?php do_action( 'entry_content' ); ?>
                     <?php do_action( 'entry_meta' ); ?>
                     <?php do_action( 'entry_comment' ); ?>
-		</article>
-           <?php endwhile; 
-	else: ?>
+		    </article>
+		<?php endwhile; 
+	    else: ?>
 		<p><?php _e('Sorry, this page does not exist.','txt_kobotolo'); ?></p>
-	<?php endif; ?>
-  </div>
-  <div class="col-md-4">
-	<?php get_sidebar(); ?>  	
-  </div>
+	    <?php endif; ?>
+	</div>
+	    <?php do_action('sidebar_right'); ?>  	
+    </div>
 </div>
-	</div></div>
 
 <?php get_footer(); ?>
