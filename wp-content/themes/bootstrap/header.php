86a47f9e7ba4ace7029e669555331f820361b3ce
kaolss <karin@kobotolo.se>
2014-09-12 13:50:36 +0200

    boxed layout

--- wp-content/themes/bootstrap/header.php
+++ wp-content/themes/bootstrap/header.php
@@ -1,4 +1,6 @@
 <?php ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
 <meta charset="utf-8">
 <title><?php wp_title( '&#124;', true, 'right' ); ?></title>
@@ -9,41 +11,24 @@
 <!--[if lt IE 9]>
   <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-<?php wp_enqueue_script("jquery"); ?>
-<?php wp_head(); ?>
+<?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <div class="site-container container-fluid">	
+    <div class="site-container">	
         <div class="site-header">	
-            <?php $header_image = get_header_image();
-            if ( ! empty( $header_image ) ) { ?>
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                   <img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="
-                <?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a><?php 
-                    
-            }?>
-            <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-            <nav class="navbar navbar-default" role="navigation">
-                <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                   
-
-        <?php
-            wp_nav_menu( array(
-                'menu'              => 'main',
-                'theme_location'    => 'main',
-                'depth'             => 2,
-                'container'         => 'div',
-                'container_class'   => 'menu nav-primary menu-primary',
-	        'container_id'      => '',
-                'menu_class'        => 'menu-main menu-primary nav navbar-nav container'
-         
-             
-            
-	      ));
-        ?>
-    </div>
-</nav>
+	    <nav class="nav-primary navbar-default" role="navigation">
+		 <?php wp_nav_menu( array(
+		    'theme_location'    => 'primary',
+		    'depth'             => 2,
+		    'container'         => 'div',
+		    'container_class'   => 'container',
+		    'container_id'      => '',
+		    'menu_class'        => 'menu-main menu-primary'));
+		 ?>
+	    </nav>
+	    <div class="container">	
+		<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+            </div>
 	</div>
-	
+    <div class="site-content">	
 
