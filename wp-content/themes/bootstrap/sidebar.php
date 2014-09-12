d3b0e261fdcc8f405ced84a741ed5acfb97847cf
kaolss <karin@kobotolo.se>
2014-09-12 14:06:01 +0200

    left or right sidebar

--- wp-content/themes/bootstrap/sidebar.php
+++ wp-content/themes/bootstrap/sidebar.php
@@ -3,25 +3,7 @@
 // Exit if accessed directly
 if( !defined( 'ABSPATH' ) ) {
 	exit;
-}
-
-/**
- * Main Widget Template
- *
- *
- * @file           sidebar.php
- * @package        Responsive
- * @author         Emil Uzelac
- * @copyright      2003 - 2014 CyberChimps
- * @license        license.txt
- * @version        Release: 1.0
- * @filesource     wp-content/themes/responsive/sidebar.php
- * @link           http://codex.wordpress.org/Theme_Development#Widgets_.28sidebar.php.29
- * @since          available since Release 1.0
- */
-
-/*
- * Load the correct sidebar according to the page layout
- */
-   dynamic_sidebar( 'home_right_1' ); 
-
+}?>
+<div  class="sidebar">
+   <?php dynamic_sidebar( 'home_right_1' ); ?>
+</div>
