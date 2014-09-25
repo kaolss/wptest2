68dbc034cada69fd0efc33c0c3de010f786dcff9
kaolss <karin@kobotolo.se>
2014-09-25 08:42:15 +0200

    not ready

--- wp-content/themes/bootstrap/lib/cls_inittheme.php
+++ wp-content/themes/bootstrap/lib/cls_inittheme.php
@@ -11,23 +11,55 @@ add_action( 'wp_head', 'kobotolo_head' );
 /*****************************************************************************
 ** kobotolo_head
 *****************************************************************************/
-function kobotolo_head (){
-    global $my_Admin;
-    if ($my_Admin->get_option_value('boxed')==='checked') { ?>
-        <style>
+function kobotolo_head (){ ?>
+    <style>
+    <?php global $my_Admin;
+    $primary = $my_Admin->get_option_value('page_bg');    
+
+    switch ( $primary) {
+	case 'ligt' : ?>
+		body {background: whitesmoke;
+		    color:black;}
+	    <?php break;
+	case 'medium' : ?>
+		body {background: grey;
+		color:black;}
+	    <?php break;
+	default: ?>
+		body {background: black;
+		    color:grey;}
+    <?php }    ?>
+		
+    
+    <?php    if ($my_Admin->get_option_value('boxed')==='checked') { ?>    
 	    body {
-		background: <?php echo $my_Admin->get_option_value('color_scheme')?>;
+		background: <?php echo $my_Admin->get_option_value('page_bg')?>;
 	    } 
 	    .site-container {
-		width:1170px;
+		max-width:1170px;
 		margin:0 auto;
-		border-left:10px black solid;
-		border-right:10px black solid;
+		border-left:2px black solid;
+		border-right:2px black solid;
 	    }
-	</style>
+    	
     <?php    }
+    $secondary = $my_Admin->get_option_value('secondary');    
+
+    switch ( $secondary) {
+	case 'red' :?>
+	    a {color:red;
+	    }
+	    .btn-primary {
+color: #fff;
+background-color: red;//#428bca;
+border-color: darkred;//#357ebd;
+}
+	    <?php break;
+	default:
+    }?>
     
-    ?>
+    
+	</style>
     <style>
 	#rememberNav nav, div#offcanvas nav {
 	/************ add height for menu here *******/
@@ -36,17 +68,18 @@ function kobotolo_head (){
     </style>
     
     <?php 
-    echo 'testing sidebar';
+//    echo '<br>testing sidebar';
     $sidebar= $my_Admin->get_option_value('sidebar_pos');
+    _log("sidebar general placering =>".$sidebar);
     if (is_page()) {  
-	echo 'is page';
+	_log( 'is page');
     	$meta = get_post_meta(get_the_ID(), 'page');
 	if (isset($meta[0])) {
-	    var_dump($meta[0]);
 		$tag_meta = $meta[0]; 
-	    $sidebar=$tag_meta['sidebar']?:3;   echo 'sidebar'.$sidebar; 
+		//_log($tag_meta['sidebarplace']);
+	    $sidebar=$tag_meta['pagesidebar']!='Theme'? $pagesidebar:$tag_meta['sidebar'] ;   
 	}}
-    
+    _log("sidebar placering =>".$sidebar);
     switch ( $sidebar) {
 	case 'left':
 	    add_action('sidebar_left', 'kobotolo_sidebar', 10);
