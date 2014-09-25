317880f73cd06dc3f382243b574692828bf1cc5a
kaolss <karin@kobotolo.se>
2014-09-25 09:17:32 +0200

    not ready

--- wp-content/themes/bootstrap/footer.php
+++ wp-content/themes/bootstrap/footer.php
@@ -1,29 +1,27 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 ?>
-<div class="row before_footer">
-    <div class="container">
-        <div class="col-md-4">
-	    <?php dynamic_sidebar( 'footer_left' ); ?>
-	</div>	
-	<div class="col-md-4">
-	    <?php dynamic_sidebar( 'footer_center' ); ?>
-	</div>	
-	<div class="col-md-4">
-	    <?php dynamic_sidebar( 'footer_right' );?>
-	</div>
-	<hr>
-    </div>
-</div>
 <footer>
-    <div class="container">
-	<div class="col-md-12">
-	<?php do_action('kobotolo_footer'); ?>
+    <div class="container-fluid">
+	<div class="row footerarea">
+	    <div class="container">
+	        <div class="col-md-4">
+		   <?php dynamic_sidebar( 'footer_left' ); ?>
+		</div>	
+		<div class="col-md-4">
+		    <?php dynamic_sidebar( 'footer_center' ); ?>
+		</div>	
+		<div class="col-md-4">
+			<?php dynamic_sidebar( 'footer_right' );?>
+		</div>
+	    </div>
+	    <div class="container">
+		<div class="row copyrightarea">
+		    <div class="col-md-12">
+			<?php do_action('kobotolo_footer'); ?>
+		    </div> <!-- /container -->
+		</div> <!-- /container -->
+	    </div> <!-- /container -->
 	</div> <!-- /container -->
     </div> <!-- /container -->
 </footer>
