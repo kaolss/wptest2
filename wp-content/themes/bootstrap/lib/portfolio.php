f41416a8224057005b1c44330580f876a8d0ec69
kaolss <karin@kobotolo.se>
2014-09-25 09:15:31 +0200

    not ready

--- wp-content/themes/bootstrap/lib/portfolio.php
+++ wp-content/themes/bootstrap/lib/portfolio.php
@@ -5,25 +5,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+global $my_Admin;
+$x= $my_Admin->get_option_value('sidebar_pos');
+$Meta_fieldsxx = array ('portfolio_categories'=> 
+	    array('type'=>'Taxonomy'
+		,'helptext'=>__('Which portfolio filters should be included?','txt_kobotolo')
+		,'label'=>__('Select filters for portfolio','txt_kobotolo')
+		,'default'=>''));     
+
+$Metaxx = new Cls_Meta(
+    array('kobotolo_portfolio')
+    ,__('Portfolio Filter settings','txt_kobotolo'), 'special', $Meta_fieldsxx, 'kobotolo_portfolio_page_filter');  
+
 
 $Meta_fields = array (
     'number' =>array('type' =>'text'
 		,'label'=>__('How many images in rows (3, 4 eller 5)','txt_kobotolo')
-		,'default'=>'3')
-    ,'portfolio_categories'=> array('type'=>'special'
-		,'what'=>'Taxonomy'
-		,'label'=>__('Select filters for portfolio','txt_kobotolo')
-		,'default'=>''));     
-$Meta = new Cls_Meta('Portfolio page settings', 'page', $Meta_fields, 'kobotolo_portfolio');  
+		,'helptext'=>__('How many images in rows (3, 4 eller 5)','txt_kobotolo')
+		,'default'=>'3'));     
+
+$Meta = new Cls_Meta(
+    array( 'kobotolo_portfolio')
+    ,__('Portfolio page settings','txt_kobotolo'), 'page', $Meta_fields, 'kobotolo_portfolio_page');  
+
 
 $Meta_fields2 = array(
     'link'=>array('type'=>'text'
-      , 'label'=>__('Link on click','txt_kobotolo')
-      ,'default'=>'vvvvvvv'));
-$Meta2 = new Cls_Meta('Portfolio settings','post_type',$Meta_fields2, 'portfolio_kobotolo');  
+      , 'label'=>__('Link on click on portfolio page','txt_kobotolo')
+      , 'helptext'=>__('If nothing enter - porrtfolio post will be opened (this post)','txt_kobotolo')
+      ,'default'=>''));
+$Meta2 = new Cls_Meta('portfolio_kobotolo',__('Portfolio settings','txt_kobotolo'),'post_type',$Meta_fields2, 'portfolio_kobotolo');  
 
+
+global $wp_registered_sidebars;
+global $sbg;
 $Meta_fields3 = array (
-    'sidebar'=>array('type'=>'text'
+    'pagesidebar'=>array('type'=>'select'
       ,'label'=>__('Place for sidebar','txt_kobotolo')
-      ,'default'=>'right'));
-$Meta3 = new Cls_Meta('Page settings', 'page',$Meta_fields3, 'page' );
+      ,'helptext'=>__('Use => Left, Right, None or Theme','txt_kobotolo')
+      ,'values'=> array('Theme', 'Left', 'Right','None')
+      ,'default'=> 'Theme')
+ ,'choosensidebar'=>array('type'=>'select'
+      ,'label'=>__('Select sidebar','txt_kobotolo')
+      ,'values'=> $sbg->get_sidebars()
+      ,'helptext'=>__('Use => Left, Right, None or Theme','txt_kobotolo')
+      ,'default'=>'Theme')
+     );
+$Meta3 = new Cls_Meta(array('page'),
+        $hedaer=__('Page settings','txt_kobotolo'), 
+	$type='page',
+	$meta=$Meta_fields3, 
+	$DB_field='kobotolo_page' );
